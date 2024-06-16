e651853d23da4da2cbdcd7657bb2522588901775
Fabricio Lancieri <fabriciolancieri@gmail.com>
2024-06-16 13:49:59 +0200

    link refactor and remove useless assets

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,11 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-        navigate('/');
-    };
-
     return (
         <header className="bg-white border-b border-gray-300 px-4 py-2 mx-4 flex items-center justify-between sticky top-0 z-50">
-            <h1 onClick={handleClick} className="text-lg font-bold text-blue-400">Podcaster</h1>
+            <Link to={'/'}>
+                <h1 className="text-lg font-bold text-blue-400">Podcaster</h1>
+            </Link>
         </header>
     );
 };
