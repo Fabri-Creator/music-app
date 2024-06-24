5bc2c3751ad2bf721756af3e0aecf7e564ecbdad
Fabricio Lancieri <fabriciolancieri@gmail.com>
2024-06-24 15:45:46 +0200

    test(podcast-list): refactor waitFor uses

--- src/__test__/PodcastList.test.tsx
+++ src/__test__/PodcastList.test.tsx
@@ -10,7 +10,7 @@ const mocks = vi.hoisted(() => ({
 }));
 
 beforeAll(() => {
-    vi.doMock('../hooks/usePodcasterData', () => ({
+    vi.doMock("../hooks/usePodcasterData", () => ({
         usePodcasterData: mocks.usePodcasterData,
     }));
 });
@@ -25,35 +25,58 @@ describe("PodcastList", () => {
 
         render(<PodcastList />);
 
-        waitFor(() => expect(screen.getByText(/The Joe Budden Podcast - The Joe Budden Network/i)).toBeInTheDocument());
-        waitFor(() => expect(screen.getByText(/A History of Rock Music in 500 Songs/i)).toBeInTheDocument());
+        waitFor(() => {
+            expect(
+                screen.getByText(/The Joe Budden Podcast - The Joe Budden Network/i)
+            ).toBeInTheDocument();
+            expect(
+                screen.getByText(/A History of Rock Music in 500 Songs/i)
+            ).toBeInTheDocument();
+        });
     });
 
     it("filters podcasts based on search text", async () => {
         mocks.usePodcasterData.mockReturnValue([mockPodcastData, false, false]);
         render(<PodcastList />);
 
-        waitFor(() => screen.getByText(/The Joe Budden Podcast - The Joe Budden Network/i));
-        waitFor(() => screen.getByText(/A History of Rock Music in 500 Songs/i));
-        waitFor(() => screen.getByPlaceholderText(/Search podcasts/i));
+        waitFor(() => {
+            expect(
+                screen.getByText(/The Joe Budden Podcast - The Joe Budden Network/i)
+            ).toBeInTheDocument();
+            expect(
+                screen.getByText(/A History of Rock Music in 500 Songs/i)
+            ).toBeInTheDocument();
+        });
 
+        // Wait for filtered results
         waitFor(() => {
             const searchInput = screen.getByPlaceholderText(/Search podcasts/i);
             expect(searchInput).toBeInTheDocument();
 
             // Filter for "The Joe Budden"
             fireEvent.change(searchInput, { target: { value: "The Joe Budden" } });
+            expect(
+                screen.getByText(/The Joe Budden Podcast - The Joe Budden Network/i)
+            ).toBeInTheDocument();
+            expect(
+                screen.queryByText(/A History of Rock Music in 500 Songs/i)
+            ).toBeNull();
+        });
 
-            // Wait for filtered results
-            expect(screen.getByText(/The Joe Budden Podcast - The Joe Budden Network/i)).toBeInTheDocument();
-            expect(screen.queryByText(/A History of Rock Music in 500 Songs/i)).toBeNull();
+        // Clear filter
 
-            // Clear filter
+        // Wait for cleared results
+        waitFor(() => {
+            const searchInput = screen.getByPlaceholderText(/Search podcasts/i);
+            expect(searchInput).toBeInTheDocument();
+
             fireEvent.change(searchInput, { target: { value: "" } });
-
-            // Wait for cleared results
-            expect(screen.getByText(/The Joe Budden Podcast - The Joe Budden Network/i)).toBeInTheDocument();
-            expect(screen.getByText(/A History of Rock Music in 500 Songs/i)).toBeInTheDocument();
+            expect(
+                screen.getByText(/The Joe Budden Podcast - The Joe Budden Network/i)
+            ).toBeInTheDocument();
+            expect(
+                screen.getByText(/A History of Rock Music in 500 Songs/i)
+            ).toBeInTheDocument();
         });
     });
 
@@ -66,6 +89,8 @@ describe("PodcastList", () => {
     it("shows error message when there is an error fetching data", async () => {
         mocks.usePodcasterData.mockReturnValue([null, false, true]);
         render(<PodcastList />);
-        waitFor(() => expect(screen.getByText(/Error loading data/i)).toBeInTheDocument());
+        waitFor(() =>
+            expect(screen.getByText(/Error loading data/i)).toBeInTheDocument()
+        );
     });
 });
